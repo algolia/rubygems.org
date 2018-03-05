8d5c5c0d6fb750cc164378fd548a4ed3fa9effa5
Raphael Daguenet <raphael.daguenet@gmail.com>
2018-03-05 17:37:30 +0100

    Fallback on summary when description is empty

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -27,6 +27,9 @@ search.addWidget(
       templates: {
         empty: 'No results',
         item: function(hit) {
+          var desc = (hit._snippetResult.description.value ? 
+            hit._snippetResult.description.value :
+            hit._snippetResult.summary.value);
           var licenses = '';
 
           for (var i = 0; i < hit.licenses.length; i++) {
@@ -45,7 +48,7 @@ search.addWidget(
                   '<div class="ais-hit-licenses">' + licenses + '</div>\
                   <span class="gems__gem__version">' + hit.number + '</span>\
                 </h2>\
-                <p class="gems__gem__desc t-text">' + hit._snippetResult.description.value + '</p>\
+                <p class="gems__gem__desc t-text">' + desc + '</p>\
               </span>\
               <p class="gems__gem__downloads__count">' + numberWithCommas(hit.downloads) + '\
                 <span class="gems__gem__downloads__heading">Downloads</span>\
